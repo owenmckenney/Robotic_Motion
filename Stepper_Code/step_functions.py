76128ee0df4a2b2e94ec96ddb7aa3354d02cf2b8
owenmckenney <owen.mckenney1@gmail.com>
2020-12-22 21:44:19 -0500

    cleaned up

--- Stepper_Code/step_functions.py
+++ Stepper_Code/step_functions.py
@@ -29,57 +29,75 @@ class Stepper:
                       "1/16": (0,0,1),
                       "1/32": (1,0,1)}
         GPIO.output(self.mode_pins, resolution[self.step_type]) 
+    
+    def step(self, delay):
+        GPIO.output(self.step_pin, GPIO.HIGH)
+        time.sleep(delay)
+        GPIO.output(self.step_pin, GPIO.LOW)
+        time.sleep(delay)
 
     def Rotate(self, rotations, d):
         GPIO.output(self.dir_pin, d)
         
         for x in range(self.spr * rotations):
-            GPIO.output(self.step_pin, GPIO.HIGH)
-            time.sleep(self.delay)
-            GPIO.output(self.step_pin, GPIO.LOW)
-            time.sleep(self.delay)
+           self.step(self.delay)
         
         GPIO.cleanup()
 
+    def Accel(self, accel_point, accel_delay, d, s):
+        increment = (self.delay / accel_delay) ** (1 / (accel_point - 1))
+
+        for x in range(s):
+            if x < accel_point - 1 or x > s - accel_point:
+                self.step(accel_delay)
+                    
+                if x < accel_point - 1:
+                    accel_delay = accel_delay * increment
+                    print("Accelerated", str(round(accel_delay, 4)), str(d))
+                else: 
+                    accel_delay = accel_delay / increment
+                    print("De-accelerated", str(round(accel_delay, 4)), str(d))
+            else:
+                self.step(accel_delay)
+                print("Constant", str(round(accel_delay, 4)), str(d))
+
     def Rotate_Ramp_Up_Down(self, rotations, d):
         GPIO.output(self.dir_pin, d)
-        #accel_point = (self.spr / 4) * rotations
-        accel_point = 50
-        #accel_delay = self.delay * accel_point
-        accel_delay = 0.025
-        delay_increment = accel_delay / 50
-
-        #.025
-
-        print(accel_point, accel_delay)
+        accel_point = self.spr / 64 * rotations 
+        accel_delay = 0.05
+        self.Accel(accel_point, accel_delay, d, self.spr * rotations)
         
-        
-        for x in range(self.spr * rotations):
+    def Oscilate(self, instances):
+        GPIO.output(self.dir_pin, self.ccw)
+        accel_point = self.spr / 200
+        accel_delay = 0.05
+        increment = (self.delay / accel_delay) ** (1 / (accel_point - 1))
+        direction = self.ccw
+        print(accel_delay, increment)
+        self.Accel(accel_point, accel_delay, direction, int(self.spr / 32))
 
-            if x < accel_point or x > (self.spr * rotations) - accel_point:
-                GPIO.output(self.step_pin, GPIO.HIGH)
-                time.sleep(accel_delay)
-                GPIO.output(self.step_pin, GPIO.LOW)
-                time.sleep(accel_delay)
-                
-                if x < accel_point - 1:
-                    accel_delay = accel_delay - (delay_increment)
-                    print("Accelerated " + str(accel_delay))
-                else:
-                    accel_delay = accel_delay + delay_increment
-                    print("De-accelerated " + str(accel_delay))
-            
+        accel_delay = 0.05
+        GPIO.output(self.dir_pin, self.cw)
+        direction = self.cw
+
+        print("done with first")
+ 
+        for x in range(instances):
+            self.Accel(accel_point, accel_delay, direction, int(self.spr / 16))
+
+            if direction == self.ccw:
+                direction = self.cw
+                GPIO.output(self.dir_pin, self.cw)
             else:
-                GPIO.output(self.step_pin, GPIO.HIGH)
-                time.sleep(self.delay)
-                GPIO.output(self.step_pin, GPIO.LOW)
-                time.sleep(self.delay)
-                print("Regular " + str(self.delay))
+                direction = self.ccw
+                GPIO.output(self.dir_pin, self.ccw)
 
+        self.Accel(accel_point, accel_delay, direction, int(self.spr / 32))
 
-
+        
 stepper1 = Stepper(20, 21, 16, 3200, (1,7,8), "Full")
 #stepper1.Rotate(1, 1)
-stepper1.Rotate_Ramp_Up_Down(1, 1)
+#stepper1.Rotate_Ramp_Up_Down(1, 1)
+stepper1.Oscilate(4)
 
 
